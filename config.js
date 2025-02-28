937c7f3dacc6eedfcb776e587eb500e48dce86c4
DINUWH-MD <dinethnethruvan772@gmail.com>
2025-02-28 12:58:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "rKpX1IqY#M4idQSmLAmDs00Tmj3xPawx8_9x1RnNy1OtKBcENqhc",
+  SESSION_ID: process.env.SESSION_ID || "LUthmRhT#CgzgCXDCZVtxOCFvuDiwYboIXNArBMC5GPZ3wiU8pdA",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
